268db5cf4fc74f84a6a9659d0e38679a9c8fefaf
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2023-11-07 09:03:19 -0800

    fix duplicates

--- app/forms/review_form 2.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, ValidationError
-
-class ReviewForm(FlaskForm):
-    review = TextAreaField("Review", validators=[DataRequired()])
-    submit = SubmitField("Submit")
